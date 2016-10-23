fb049b7905f9674e8fbfd8317e4fd58180f99c3a
arosenb4 <arosenb4@vols.utk.edu>
2016-10-23 01:57:47 -0400

    Skeleton Design Complete, Dygraphs Complete, Mobile 1st complete

--- skeleton/js/script.js
+++ skeleton/js/script.js
@@ -27,22 +27,21 @@ function processData(allText) {
     var allTextLines = allText.split(/\r\n|\n/);
     var headers = allTextLines[0].split(',');
     var lines = [];
-		var tablearea = document.getElementById('tablearea'),
-				table = document.createElement('table');
+		var table = document.getElementById('tablearea');
 
+    /*for (var i=1; i<allTextLines.length; i++) {*/
     for (var i=1; i<allTextLines.length; i++) {
+    /*for (var i=1; i<9; i++) {*/
         var data = allTextLines[i].split(',');
-				var tr = document.createElement('tr');
         if (data.length == headers.length) {
-						tr.appendChild( document.createElement('td') );
-						tr.appendChild( document.createElement('td') );
+            var tr = document.createElement('tr');
             for (var j=0; j<headers.length; j++) {
+						  tr.appendChild( document.createElement('td') );
 							tr.cells[j].appendChild( document.createTextNode(data[j]) );
             }
 					table.appendChild(tr);
         }
     }
-		tablearea.appendChild(table);
     //alert(lines);
 }
 
