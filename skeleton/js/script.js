6cf4665a2bdd53f7d3b3d5e4cac55d83146023b8
arosenb4 <arosenb4@vols.utk.edu>
2016-10-23 00:46:54 -0400

    Import CSV to simple table

--- skeleton/js/script.js
+++ skeleton/js/script.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+
       new Dygraph(
           document.getElementById("noroll"),
           "../assets/temperatures.csv",
@@ -11,5 +12,40 @@ $(document).ready(function () {
             showRangeSelector: true
           }
       );
+
+		$.ajax({
+        type: "GET",
+        url: "assets/temperatures.csv",
+        dataType: "text",
+        success: function(data) {processData(data);}
+     });
+
+});
+
+
+function processData(allText) {
+    var allTextLines = allText.split(/\r\n|\n/);
+    var headers = allTextLines[0].split(',');
+    var lines = [];
+		var tablearea = document.getElementById('tablearea'),
+				table = document.createElement('table');
+
+    for (var i=1; i<allTextLines.length; i++) {
+        var data = allTextLines[i].split(',');
+				var tr = document.createElement('tr');
+        if (data.length == headers.length) {
+						tr.appendChild( document.createElement('td') );
+						tr.appendChild( document.createElement('td') );
+            for (var j=0; j<headers.length; j++) {
+							tr.cells[j].appendChild( document.createTextNode(data[j]) );
+            }
+					table.appendChild(tr);
+        }
     }
-);
+		tablearea.appendChild(table);
+    //alert(lines);
+}
+
+
+
+
